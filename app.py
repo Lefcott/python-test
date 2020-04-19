98f4cad3b9bcd08c03456d131a037304cbec49ea
Leandro Cotti <leandro.cotti@widergy.com>
2020-04-19 16:33:11 -0300

    wip

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html", multichannel='MULTIchannel')
+
+
+@app.route('/favicon.png')
+def favicon():
+    return render_template("favicon.png")
+@app.route('/style.css')
+def style():
+    return render_template("style.css")
+
+# @app.route('/')
+# def index():
+#     return "Hello World!"
+
 
 if __name__ == '__main__':
     app.run()
